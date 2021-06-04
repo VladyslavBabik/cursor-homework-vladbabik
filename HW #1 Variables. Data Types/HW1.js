81e83a1a983468e21855528bf78bb9c192539c27
Vlad <vladyslavbabik@gmail.com>
2021-06-04 11:37:35 +0300

    HW#1 Fixed2

--- HW #1 Variables. Data Types/HW1.js
+++ HW #1 Variables. Data Types/HW1.js
@@ -1,41 +1,42 @@
 // base
-const Bread = 15.678;
-const Cheese = 123.965;
-const Meat = 90.2345;
-const MaxNumber = Math.max(Bread, Cheese, Meat);
-const MinNumber = Math.min(Bread, Cheese, Meat);
-const Sum = Bread + Cheese + Meat;
-const WithoutFractions = Math.floor(Bread) + Math.floor(Cheese) + Math.floor(Meat);
-const SumRoundedToHundreds = (Math.round(Bread / 100) + Math.round(Cheese / 100) + Math.round(Meat / 100)) * 100;
-const BulianSum = WithoutFractions % 2 == 0;
-const Rest = 500 - Sum;
-const ArithmeticMean = Math.round(((Bread + Cheese + Meat) / 3) * 100) / 100;
-const RandomDiscount = Math.random();
-const RandomDiscountFix = Math.round(RandomDiscount * 10000) / 100;
-const AmountPay = Math.round((Sum - Sum * RandomDiscount) * 100) / 100;
-const Profit = Math.round(((Sum / 2) - (Sum * RandomDiscount)) * 100) / 100;
-console.log("Максимальне число: " + MaxNumber);
-console.log("Мінімальне число: " + MinNumber);
-console.log("Сума: " + Sum);
-console.log("Сума без дробів: " + WithoutFractions);
-console.log("Сума заокруглена до сотень: " + SumRoundedToHundreds);
-console.log("Число парне: " + BulianSum);
-console.log("Решта з 500: " + Rest);
-console.log("Середнє арифметичне: " + ArithmeticMean);
-console.log("Рандомна знижка: " + RandomDiscountFix);
-console.log("Сума виплати: " + AmountPay);
-console.log("Прибуток: " + Profit);
+const breadPrice = 15.678;
+const cheesePrice = 123.965;
+const meatPrice = 90.2345;
+const cash = 500;
+const maxNumber = Math.max(breadPrice, cheesePrice, meatPrice);
+const minNumber = Math.min(breadPrice, cheesePrice, meatPrice);
+const sumPrice = breadPrice + cheesePrice + meatPrice;
+const withoutFractions = Math.floor(breadPrice) + Math.floor(cheesePrice) + Math.floor(meatPrice);
+const sumRoundedToHundreds = (Math.round(breadPrice / 100) + Math.round(cheesePrice / 100) + Math.round(meatPrice / 100)) * 100;
+const bulianSum = withoutFractions % 2 == 0;
+const restCash = cash - sumPrice;
+const arithmeticMean = Math.round(((breadPrice + cheesePrice + meatPrice) / 3) * 100) / 100;
+const randomDiscount = Math.random();
+const randomDiscountFix = Math.round(randomDiscount * 10000) / 100;
+const amountPay = Math.round((sumPrice - sumPrice * randomDiscount) * 100) / 100;
+const profit = Math.round(((sumPrice / 2) - (sumPrice * randomDiscount)) * 100) / 100;
+console.log("Максимальне число: " + maxNumber);
+console.log("Мінімальне число: " + minNumber);
+console.log("Сума: " + sumPrice);
+console.log("Сума без дробів: " + withoutFractions);
+console.log("Сума заокруглена до сотень: " + sumRoundedToHundreds);
+console.log("Число парне: " + bulianSum);
+console.log("Решта з 500: " + restCash);
+console.log("Середнє арифметичне: " + arithmeticMean);
+console.log("Рандомна знижка: " + randomDiscountFix);
+console.log("Сума виплати: " + amountPay);
+console.log("Прибуток: " + profit);
 
 //advanced
 document.writeln(`
-  Максимальне число: ${MaxNumber} <br/>
-  Мінімальне число: ${MinNumber} <br/>
-  Сума: ${Sum} <br/>
-  Сума без дробів: ${WithoutFractions} <br/>
-  Сума заокруглена до сотень: ${SumRoundedToHundreds} <br/>
-  Число парне: ${BulianSum} <br/>
-  Решта з 500: ${Rest} <br/>
-  Середнє арифметичне: ${ArithmeticMean} <br/>
-  Рандомна знижка: ${RandomDiscountFix} <br/>
-  Сума виплати: ${AmountPay} <br/>
-  Прибуток: ${Profit}`)
+  Максимальне число: ${maxNumber} <br/>
+  Мінімальне число: ${minNumber} <br/>
+  Сума: ${sumPrice} <br/>
+  Сума без дробів: ${withoutFractions} <br/>
+  Сума заокруглена до сотень: ${sumRoundedToHundreds} <br/>
+  Число парне: ${bulianSum} <br/>
+  Решта з 500: ${restCash} <br/>
+  Середнє арифметичне: ${arithmeticMean} <br/>
+  Рандомна знижка: ${randomDiscountFix} <br/>
+  Сума виплати: ${amountPay} <br/>
+  Прибуток: ${profit}`)
